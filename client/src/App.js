5ddfb0871fe515b717eb786636841cda3918e26f
Trần Đình Hoàng <super.royal147@gmail.com>
2023-02-07 00:14:50 +0700

    Setup Sass & Tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./assets/sass/index.scss";
 
 const App = () => {
-  return <div>Basic App</div>;
+  return (
+    <h1 className="text-3xl font-bold text-center mt-5 uppercase cursor-pointer hover:text-blue-500">
+      Hello world!
+    </h1>
+  );
 };
 
 export default App;
